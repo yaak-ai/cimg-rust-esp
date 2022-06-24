8103245191964c4c8a3ea790eaa0aba063798cd0
Maxime Borges <maxime@yaak.ai>
2022-06-24 12:48:45 +0200

    Add Rust espressif toolchain support, disable other variants

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.61/Dockerfile -t cimg/rust:1.61.0  -t cimg/rust:1.61 .
-docker build --file 1.61/node/Dockerfile -t cimg/rust:1.61.0-node  -t cimg/rust:1.61-node .
-docker build --file 1.61/browsers/Dockerfile -t cimg/rust:1.61.0-browsers  -t cimg/rust:1.61-browsers .
+docker build --file 1.61/Dockerfile -t yaaktech/cimg-rust-esp:1.61.0 -t yaaktech/cimg-rust-esp:1.61 .
+docker build --file 1.61/esp/Dockerfile -t yaaktech/cimg-rust-esp:1.61.0-esp -t yaaktech/cimg-rust-esp:1.61-esp .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/rust:1.61.0
-docker push cimg/rust:1.61
-docker push cimg/rust:1.61.0-node
-docker push cimg/rust:1.61-node
-docker push cimg/rust:1.61.0-browsers
-docker push cimg/rust:1.61-browsers
+docker push yaaktech/cimg-rust-esp:1.61
+docker push yaaktech/cimg-rust-esp:1.61.0
+docker push yaaktech/cimg-rust-esp:1.61-esp
+docker push yaaktech/cimg-rust-esp:1.61.0-esp
