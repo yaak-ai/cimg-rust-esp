bbb0e86b7573a563d1a6fe47fb29ae45344321be
Maxime Borges <maxime@yaak.ai>
2022-06-24 16:41:13 +0200

    Simplify [release]

--- build-images.sh
+++ build-images.sh
@@ -2,5 +2,3 @@
 # Do not edit by hand; please use build scripts/templates to make changes
 
 docker build --file 1.61/Dockerfile -t yaaktech/cimg-rust-esp:1.61.0 -t yaaktech/cimg-rust-esp:1.61 .
-docker build --file 1.61/esp/Dockerfile -t yaaktech/cimg-rust-esp:1.61.0-esp -t yaaktech/cimg-rust-esp:1.61-esp .
-docker build --file 1.61/aarch64/Dockerfile -t yaaktech/cimg-rust-esp:1.61.0-aarch64 -t yaaktech/cimg-rust-esp:1.61-aarch64 .
--- push-images.sh
+++ push-images.sh
@@ -2,7 +2,3 @@
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push yaaktech/cimg-rust-esp:1.61
 docker push yaaktech/cimg-rust-esp:1.61.0
-docker push yaaktech/cimg-rust-esp:1.61-esp
-docker push yaaktech/cimg-rust-esp:1.61.0-esp
-docker push yaaktech/cimg-rust-esp:1.61-aarch64
-docker push yaaktech/cimg-rust-esp:1.61.0-aarch64
