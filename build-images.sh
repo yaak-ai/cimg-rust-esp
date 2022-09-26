6af0c339261d17aef6a56af9aa05ad3d5cf681a5
diekleinekuh <markus.henschel@yaak.ai>
2022-09-26 19:10:54 +0200

    fix dependencies to a specific version and cleanup

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.61/Dockerfile -t yaaktech/cimg-rust-esp:1.61.0 -t yaaktech/cimg-rust-esp:1.61 .
+docker build --file 1.61/cargo-deb/Dockerfile -t yaaktech/cimg-rust-esp:1.61.0--cargo-deb -t yaaktech/cimg-rust-esp:1.61--cargo-deb .
--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push yaaktech/cimg-rust-esp:1.61
-docker push yaaktech/cimg-rust-esp:1.61.0
+docker push yaaktech/cimg-rust-esp:1.61--cargo-deb
+docker push yaaktech/cimg-rust-esp:1.61.0--cargo-deb
